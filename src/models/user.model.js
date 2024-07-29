a1e4aa42f76dda0393687ca98be6ab37e163e5cc
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-29 12:57:35 +0600

    white methods for access token and refresh token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,10 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken'
+import { configDotenv } from "dotenv";
+
+
+configDotenv()
 
 const userSchema = new Schema({
     username:{
@@ -62,5 +66,35 @@ userSchema.pre("save", async function(next){
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRED
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+      {
+          _id: this._id,
+          email: this.email,
+          username: this.username,
+          fullName: this.fullName
+      },
+      process.env.REFRESH_TOKEN_EXPIRED,
+      {
+          expiresIn:process.env.REFRESH_TOKEN_EXPIRED
+      }
+  )  
+  }
 
 export const User = mongoose.model("User", userSchema)
