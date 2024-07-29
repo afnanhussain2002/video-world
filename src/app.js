8d30646313eaa42f908648037ba04eed7b4d7943
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-29 08:36:17 +0600

    white some app.use on express, cors, cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+import { configDotenv } from 'dotenv';
+configDotenv()
 
 const app = express()
-
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true, limit:'16kb'}))
+app.use(express.static('public'))
+app.use(cookieParser())
 
 
 export {app};
