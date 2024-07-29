7d3f207a6d0b63c36216b39fc1bae63402cdfd9f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-29 12:50:52 +0600

    write a func for password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
 
 const userSchema = new Schema({
     username:{
@@ -50,5 +52,11 @@ const userSchema = new Schema({
     }
 
 },{timestamps:true})
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
 
 export const User = mongoose.model("User", userSchema)
