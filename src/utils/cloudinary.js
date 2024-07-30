a28e7adfa5fd1a036a6338765bbf9e3780dd8fa7
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-30 11:01:17 +0600

    write and export file upload oncloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -14,8 +14,17 @@ configDotenv()
 
 const uploadOnCloudinary = async(localFilePath) =>{
     try {
-        
+        if (!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        }
+    
+    )
+    console.log('File uploaded successfully', response);
     } catch (error) {
-        
+        fs.unlinkSync(localFilePath)
+        return null
     }
 }
+
+export {uploadOnCloudinary}
