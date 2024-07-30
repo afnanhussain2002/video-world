55e51b486f2c502335b9a32a511564b783a86299
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-30 10:44:31 +0600

    config the cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,3 +3,9 @@ import { configDotenv } from "dotenv";
 import {fs} from "fs"
 configDotenv()
 
+ // Configuration
+ cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
