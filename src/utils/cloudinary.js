d63946eb88e57e31fe7f08df4ebb2c871895cfee
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-08-03 11:43:17 +0600

    fix api response

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,31 +1,30 @@
 import { v2 as cloudinary } from "cloudinary";
 import { configDotenv } from "dotenv";
-import fs from "fs"
-configDotenv()
+import fs from "fs";
+configDotenv();
 
- // Configuration
- cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // file upload process
 
-const uploadOnCloudinary = async(localFilePath) =>{
-    try {
-        if (!localFilePath) return null;
-        const response = await cloudinary.uploader.upload(localFilePath,{
-            resource_type:"auto"
-        }
-    )
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
     // console.log('File uploaded successfully', response);
-    fs.unlinkSync(localFilePath)
-    return response
-    } catch (error) {
-        fs.unlinkSync(localFilePath)
-        return null
-    }
-}
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
 
-export {uploadOnCloudinary}
+export { uploadOnCloudinary };
